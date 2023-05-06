0d27625ab5d57105a307040f5e488e084cc502e5
jrbsandoval <jordanbuitragos@gmail.com>
2023-05-06 12:36:55 -0500

    pointers

---
+++ more_pointers_arrays_strings/main.h
@@ -0,0 +1,17 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+/* prototypes*/
+
+int _putchar(char);
+int _strlen(char *s);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+
+/* Librarys*/
+#include <stdio.h>
+
+
+#endif /*HEADER_H*/

365626f69c71b82810ffeab430897701507501c1
jrbsandoval <jordanbuitragos@gmail.com>
2023-05-16 15:31:02 -0500

    encode1337

---
+++ more_pointers_arrays_strings/7-leet.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * leet- function that encodes a string into 1337.
+ * @s: parameter type pointer to char
+ *
+ * Requirement: -can only use one if in your code
+ *		-only use two loops in your code
+ *		-not allowed to use switch
+ *		-not allowed to use any ternary operation
+ *
+ * Return: pointer to char
+ */
+
+char *leet(char *s)
+{
+	int i, len, j;
+	char array[] = {'a', 'e', 'o', 't', 'l'};
+	char arrayTwo[] = {'A', 'E', 'O', 'T', 'L'};
+	char patron[] = {'4', '3', '0', '7', '1'};
+
+	/*cantidad de elementos a comparar*/
+	len = 5;
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		for (j = 0; j < len; j++)
+		{
+			if (*(s + i) == array[j] || *(s + i) == arrayTwo[j])
+				*(s + i) = patron[j];
+		}
+	}
+
+	return (s);
+}

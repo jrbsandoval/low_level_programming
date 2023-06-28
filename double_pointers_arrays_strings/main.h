2468d809692d9d0fee821cb81debc220d2fd7236
jrbsandoval <jordanbuitragos@gmail.com>
2023-06-28 18:43:18 -0500

    ya

---
+++ double_pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+/*Librerias*/
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+
+/* Prototypes*/
+#include <stdio.h>
+
+#endif /* HEADER_FILE*/

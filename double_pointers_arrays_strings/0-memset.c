a29e9f1962b8249c1f8d76fa318fe08c95a11c0b
jrbsandoval <jordanbuitragos@gmail.com>
2023-06-28 17:40:16 -0500

    doublePointer

---
+++ double_pointers_arrays_strings/0-memset.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* _memset- function that fills memory with a constant byte. 
+* @s: buffer. type date pointer to char
+* @b: constant byte. Type date char
+* @n: number of buffer to fills
+*
+* Return: an pointer to buffer resultant
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+  unsigned int i;
+  char **ptr;
+
+  ptr = &s;
+
+  for (i = 0; i < n; i++)
+  {
+    *(*ptr + i) = b;
+  }
+
+  return (*ptr - i);
+}
